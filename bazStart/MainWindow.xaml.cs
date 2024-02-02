97e6f05e257a53a6ed26a0ab4db4f720c736b236
bazunaka <bazunaka32@gmail.com>
2024-02-02 18:46:41 +0300

    unzip_nginx

---
+++ bazStart/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace bazStart
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string[] files = Directory.GetFiles(Directory.GetCurrentDirectory() + "\\downloads/");
+        
+        public MainWindow()
+        {
+            InitializeComponent();           
+            FileInfo fileInf = new FileInfo(files[0]);
+            nginx_name.Content = fileInf.Name;
+        }
+
+        private void unzip_nginx_Click(object sender, RoutedEventArgs e)
+        {
+            FileInfo fileInf = new FileInfo(files[0]);
+            string zip_string = files[0];
+            ZipFile.ExtractToDirectory(zip_string, Directory.GetCurrentDirectory());
+        }
+    }
+}
